a7f1b79e0171379bd0f75bb0ed1ff98f3657d9db
I-am-Myxi <mail@myxi.tk>
2021-11-02 22:23:30 +0530

    Added decorators.
    Easier to add new commands and code looks cleaner than before.

---
+++ main.py
@@ -0,0 +1,66 @@
+import argparse
+import os
+from simpleeval import simple_eval
+from typing import Callable
+
+
+class Argparse:
+    class FunctionInfo:
+        def __init__(self, name: str, instance: Callable, kwargs: dict) -> None:
+            self.name = name
+            self.instance = instance
+            self.kwargs = kwargs
+
+    def __init__(self, **kwargs) -> None:
+        self.functions = list()
+        self.parser = argparse.ArgumentParser(**kwargs)
+
+    def add_command(self, **kwargs) -> Callable:
+        def inner(func):
+            self.functions.append(self.FunctionInfo(func.__name__, func, kwargs))
+
+        return inner
+
+    def start(self) -> None:
+        for function in self.functions:
+            dest = function.kwargs.get("dest") or function.name
+            self.parser.add_argument(dest, **function.kwargs)
+        args = self.parser.parse_args()
+        for function in self.functions:
+            dest = function.kwargs.get("dest") or function.name
+            attr = getattr(args, dest)
+            if attr:
+                function.instance(attr)
+
+
+argparse = Argparse(description="Basic but useful CLI tools.")
+
+
+@argparse.add_command(
+    metavar="<filename>", help="Create a new empty file.", dest="--mkfile"
+)
+def mkfile_command(string):
+    current_directory = os.getcwd()
+    file_directory = os.path.join(current_directory, string)
+    if os.path.exists(file_directory):
+        print("Path already exists.")
+    else:
+        open(file_directory, "w").close()
+        print("File has been created.")
+
+
+@argparse.add_command(
+    metavar="<expression>",
+    help="Safely evaluate an expression. Could be used as a basic calculator too.",
+    dest="--eval",
+)
+def eval_command(string):
+    try:
+        execution = simple_eval(string)
+        print(execution)
+    except Exception as error:
+        print(f"The code raised error.\n{error}")
+        raise error
+
+
+argparse.start()
